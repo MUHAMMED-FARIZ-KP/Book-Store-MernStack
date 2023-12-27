b0aaecd49e03454283de16af8ae3c0ec59bedf78
MUHAMMED FARIZ KP <81973124+MUHAMMED-FARIZ-KP@users.noreply.github.com>
2023-12-27 13:12:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,13 +10,13 @@ app.use(express.json())
 
 app.use(cors())
 
-// app.use(
-//     cors({
-//         origin:'http://localhost:3000',
-//         methods:['GET','POST','PUT','DELETE'],
-//         allowedHeaders:['Content-Type'],
-//     })
-// )
+app.use(
+    cors({
+        origin:'https://book-store-mern-stack-frontend-brown.vercel.app/',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+)
 app.get('/',(request,response)=>{
     //console.log(request)
     return response.status(234).send('Welcome to mern stack')
